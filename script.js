a96e8f21ee700970382b3fe31a05cd06298f3156
hiramr97 <demonic2568@gmail.com>
2022-10-25 14:57:51 -0400

    cleaned up alot of code, added the win/lose functionality. only need modal and finishing touches.

--- script.js
+++ script.js
@@ -1,169 +1,106 @@
-const board = document.getElementsByClassName("boxes-top")
-const board2 = document.getElementsByClassName("boxes-bottom")
+const pokemon = document.querySelectorAll(".card")
+let turns = 0
+let firstChoice, secondChoice;
 let isPaused = false
-let firstPick
+let resetGame = document.querySelector(".resetGame")
+let imgSpace = document.querySelectorAll(".back-view")
 
-const loadPokemon = () => {
-    const randomIds = new Set()
-    while (randomIds.size < 8) {
-        const randomNumber = Math.floor(Math.random() * 8)
-        randomIds.add(randomNumber)
-    }
-    let randomIdsArr = [...randomIds]
-    for (let i = 0; i < randomIdsArr.length; i++) {
-        if (randomIdsArr[i] === 0) {
-            box = document.querySelectorAll(".boxes-top")
-            const img = document.createElement("img")
-            img.src = "https://66.media.tumblr.com/tumblr_ma4fvrTeAk1rfjowdo1_500.gif"
-            img.id = "mons"
-            img.classList.add("rotated")
-            img.style.display = "none"
-            box[i].appendChild(img)
-        } else if (randomIdsArr[i] === 1) {
-            let box = document.querySelectorAll(".boxes-top")
-            const img = document.createElement("img")
-            img.src = "https://www.pokencyclopedia.info/sprites/gen5/ani_black-white/ani_bw_006.gif"
-            img.id = "mons"
-            img.style.display = "none"
-            box[i].appendChild(img)
-        } else if (randomIdsArr[i] === 2) {
-            let box = document.querySelectorAll(".boxes-top")
-            const img = document.createElement("img")
-            img.src = "https://www.pokencyclopedia.info/sprites/gen5/ani_black-white/ani_bw_003_m.gif"
-            img.id = "mons"
-            img.style.display = "none"
-            box[i].appendChild(img)
-        } else if (randomIdsArr[i] === 3) {
-            let box = document.querySelectorAll(".boxes-top")
-            const img = document.createElement("img")
-            img.src = "https://www.pokencyclopedia.info/sprites/gen5/ani_black-white/ani_bw_025_m.gif"
-            img.id = "mons"
-            img.style.display = "none"
-            box[i].appendChild(img)
-        } else if (randomIdsArr[i] === 4) {
-            let box = document.querySelectorAll(".boxes-top")
-            const img = document.createElement("img")
-            img.src = "https://www.pokencyclopedia.info/sprites/gen5/ani_black-white/ani_bw_144.gif"
-            img.id = "mons"
-            img.style.display = "none"
-            box[i].appendChild(img)
-        } else if (randomIdsArr[i] === 5) {
-            let box = document.querySelectorAll(".boxes-top")
-            const img = document.createElement("img")
-            img.src = "https://www.pokencyclopedia.info/sprites/gen5/ani_black-white/ani_bw_145.gif"
-            img.id = "mons"
-            img.style.display = "none"
-            box[i].appendChild(img)
-        } else if (randomIdsArr[i] === 6) {
-            let box = document.querySelectorAll(".boxes-top")
-            const img = document.createElement("img")
-            img.src = "https://www.pokencyclopedia.info/sprites/gen5/ani_black-white/ani_bw_146.gif"
-            img.id = "mons"
-            img.style.display = "none"
-            box[i].appendChild(img)
-        } else if (randomIdsArr[i] === 7) {
-            let box = document.querySelectorAll(".boxes-top")
-            const img = document.createElement("img")
-            img.src = "https://www.pokencyclopedia.info/sprites/gen5/ani_black-white/ani_bw_150.gif"
-            img.id = "mons"
-            img.style.display = "none"
-            box[i].appendChild(img)
+
+
+function flipCard(e) {
+    e.preventDefault()
+    let clickedCard = e.currentTarget
+    console.log(e.currentTarget)
+    if (clickedCard !== firstChoice && !isPaused) {
+        clickedCard.classList.add("flip")
+        if (!firstChoice) {
+            return firstChoice = clickedCard
         }
+        secondChoice = clickedCard
+        isPaused = true
+        let firstChoiceImg = firstChoice.querySelector(".back-view img").src
+        secondChoiceImg = secondChoice.querySelector(".back-view img").src
+        macthCards(firstChoiceImg, secondChoiceImg)
     }
-    const randomIds2 = new Set()
-    while (randomIds2.size < 8) {
-        const randomNumber2 = Math.floor(Math.random() * 8)
-        randomIds2.add(randomNumber2)
-    }
-    let randomIdsArr2 = [...randomIds2]
-    for (let i = 0; i < randomIdsArr2.length; i++) {
-        if (randomIdsArr2[i] === 0) {
-            let box = document.querySelectorAll(".boxes-bottom")
-            const img = document.createElement("img")
-            img.src = "https://66.media.tumblr.com/tumblr_ma4fvrTeAk1rfjowdo1_500.gif"
-            img.id = "mons-bottom"
-            img.style.display = "none"
-            box[i].appendChild(img)
-        } else if (randomIdsArr2[i] === 1) {
-            let box = document.querySelectorAll(".boxes-bottom")
-            const img = document.createElement("img")
-            img.src = "https://www.pokencyclopedia.info/sprites/gen5/ani_black-white/ani_bw_006.gif"
-            img.id = "mons-bottom"
-            img.style.display = "none"
-            box[i].appendChild(img)
-        } else if (randomIdsArr2[i] === 2) {
-            let box = document.querySelectorAll(".boxes-bottom")
-            const img = document.createElement("img")
-            img.src = "https://www.pokencyclopedia.info/sprites/gen5/ani_black-white/ani_bw_003_m.gif"
-            img.id = "mons-bottom"
-            img.style.display = "none"
-            box[i].appendChild(img)
-        } else if (randomIdsArr2[i] === 3) {
-            let box = document.querySelectorAll(".boxes-bottom")
-            const img = document.createElement("img")
-            img.src = "https://www.pokencyclopedia.info/sprites/gen5/ani_black-white/ani_bw_025_m.gif"
-            img.id = "mons-bottom"
-            img.style.display = "none"
-            box[i].appendChild(img)
-        } else if (randomIdsArr2[i] === 4) {
-            let box = document.querySelectorAll(".boxes-bottom")
-            const img = document.createElement("img")
-            img.src = "https://www.pokencyclopedia.info/sprites/gen5/ani_black-white/ani_bw_144.gif"
-            img.id = "mons-bottom"
-            img.style.display = "none"
-            box[i].appendChild(img)
-        } else if (randomIdsArr2[i] === 5) {
-            let box = document.querySelectorAll(".boxes-bottom")
-            const img = document.createElement("img")
-            img.src = "https://www.pokencyclopedia.info/sprites/gen5/ani_black-white/ani_bw_145.gif"
-            img.id = "mons-bottom"
-            img.style.display = "none"
-            box[i].appendChild(img)
-        } else if (randomIdsArr2[i] === 6) {
-            let box = document.querySelectorAll(".boxes-bottom")
-            const img = document.createElement("img")
-            img.src = "https://www.pokencyclopedia.info/sprites/gen5/ani_black-white/ani_bw_146.gif"
-            img.id = "mons-bottom"
-            img.style.display = "none"
-            box[i].appendChild(img)
-        } else if (randomIdsArr2[i] === 7) {
-            let box = document.querySelectorAll(".boxes-bottom")
-            const img = document.createElement("img")
-            img.src = "https://www.pokencyclopedia.info/sprites/gen5/ani_black-white/ani_bw_150.gif"
-            img.id = "mons-bottom"
-            img.style.display = "none"
-            box[i].appendChild(img)
+}
+
+function macthCards(firstImg, secondImg) {
+    if (firstImg === secondImg) {
+        turns++
+        if (turns == 8) {
+            setTimeout(() => {
+                alert("You won the game!")
+                return window.location.reload()
+            }, 800)
         }
+        firstChoice.removeEventListener("click", flipCard)
+        secondChoice.removeEventListener("click", flipCard)
+        firstChoice = secondChoice = ""
+        return isPaused = false
     }
+    setTimeout(() => {
+
+    }, 400)
+
+    setTimeout(() => {
+        firstChoice.classList.remove("flip")
+        secondChoice.classList.remove("flip")
+        firstChoice = secondChoice = ""
+        isPaused = false
+    }, 800)
 }
-loadPokemon()
 
-
-for (let i = 0; i < board.length; i++) {
-    board[i].addEventListener('mouseover', (event) => {
-        let pokemon = document.querySelectorAll("#mons")
-        pokemon[i].style.removeProperty("display")
-
+function shuffleBoard() {
+    turns = 0
+    firstChoice = secondChoice = ""
+    let arr = [1, 2, 3, 4, 5, 6, 7 ,8, 1, 2, 3, 4, 5, 6, 7 ,8]
+    arr.sort(() => Math.random() - 0.5)
+    console.log(arr)
+    pokemon.forEach((card, i) => {
+        card.classList.remove("flip")
+        card.addEventListener("click", flipCard)
+            if (arr[i] === 1) {
+                const img = document.createElement("img")
+                img.src = "https://66.media.tumblr.com/tumblr_ma4fvrTeAk1rfjowdo1_500.gif"
+                img.id = "blastoise"
+                imgSpace[i].appendChild(img)
+            } else if ((arr[i] === 2)) {
+                const img = document.createElement("img")
+                img.src = "https://www.pokencyclopedia.info/sprites/gen5/ani_black-white/ani_bw_006.gif"
+                imgSpace[i].appendChild(img)
+            } else if ((arr[i] === 3)) {
+                const img = document.createElement("img")
+                img.src = "https://www.pokencyclopedia.info/sprites/gen5/ani_black-white/ani_bw_003_m.gif"
+                imgSpace[i].appendChild(img)
+            } else if ((arr[i] === 4)) {
+                const img = document.createElement("img")
+                img.src = "https://www.pokencyclopedia.info/sprites/gen5/ani_black-white/ani_bw_025_m.gif"
+                imgSpace[i].appendChild(img)
+            } else if ((arr[i] === 5)) {
+                const img = document.createElement("img")
+                img.src = "https://www.pokencyclopedia.info/sprites/gen5/ani_black-white/ani_bw_144.gif"
+                imgSpace[i].appendChild(img)
+            } else if ((arr[i] === 6)) {
+                const img = document.createElement("img")
+                img.src = "https://www.pokencyclopedia.info/sprites/gen5/ani_black-white/ani_bw_145.gif"
+                imgSpace[i].appendChild(img)
+            } else if ((arr[i] === 7)) {
+                const img = document.createElement("img")
+                img.src = "https://www.pokencyclopedia.info/sprites/gen5/ani_black-white/ani_bw_146.gif"
+                imgSpace[i].appendChild(img)
+            } else if ((arr[i] === 8)) {
+                const img = document.createElement("img")
+                img.src = "https://www.pokencyclopedia.info/sprites/gen5/ani_black-white/ani_bw_150.gif"
+                imgSpace[i].appendChild(img)
     }
-    )
-}
-for (let j = 0; j < board2.length; j++) {
-    board2[j].addEventListener('mouseover', (event) => {
-        let pokemon = document.querySelectorAll("#mons-bottom")
-        pokemon[j].style.removeProperty("display")
-    }
+})}
 
-    )
+pokemon.forEach(card => {
+    card.addEventListener("click", flipCard)
+})
+
+resetGame.addEventListener("click", () => {
+    window.location.reload()
 }
-// const clickCard = (event) => {
-//     const pokemonCard = event.currentTarget
-//     const [front, back, front2, back2] = getFrontandBack(pokemonCard)
-//     front.classList.toggle
-// }
-// const getFrontandBack = (card) => {
-//     const front = document.querySelector(".boxes-top")
-//     const front2 = document.querySelector(".boxes-bottom")
-//     const back = document.querySelector("#mons-top")
-//     const back2 = document.querySelector("#mons-bottom")
-//     return [front, back, front2, back2]
-// }
+)
+shuffleBoard()
